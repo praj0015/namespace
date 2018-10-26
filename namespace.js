566de8d3fe11644538248f6f3098d00a3cf2947f
praj0015 <43075555+praj0015@users.noreply.github.com>
2018-10-26 00:28:39 -0400

    add js code

--- namespace.js
+++ namespace.js
@@ -2,31 +2,44 @@
 var PRAJ0015={
 init: function()
 {
-    let newSquareDiv=document.createElement("div");
+    let newSquareDiv = document.createElement("div");
     newSquareDiv.className="box";
     newSquareDiv.textContent="praj0015";
     
     let square=document.querySelector("#boxes");
     square.appendChild(newSquareDiv);
     
-    newSquareSection.addEventListener("click",squareClick);
-    newSquareSection.addEventListener("mouseover",mouseOver);
-    newSquareSection.addEventListener("mouseOut",mouseOut);
- 
-    function squareClick()
-{
-    this.style.borderColor="yellow";
-    this.style.backgroundColor="red";
-}
-
-function mouseOver(e)
-{
-    e.target.classList.toggle("highlight");
-}
-
-function mouseOut(e){
+    newSquareDiv.addEventListener("click",function(){
+        
+        newSquareDiv.style.borderColor="yellow";
+        newSquareDiv.style.backgroundColor="red";
+    });
+    newSquareDiv.addEventListener("mouseover",function(){
+        
+        newSquareDiv.classList.toggle("highlight");
+    });
+    newSquareDiv.addEventListener("mouseOut",function(){
+        
+        newSquareDiv.classList.toggle("highlight");
+        newSquareDiv.removeAttribute('style');
+    });
     
-    e.target.classList.toggle("highlight");
-    e.target.removeAttribute('style');
-       }
+    
+// 
+//    function squareClick()
+//{
+//    this.style.borderColor="yellow";
+//    this.style.backgroundColor="red";
+//}
+//
+//function mouseOver(e)
+//{
+//    e.target.classList.toggle("highlight");
+//}
+//
+//function mouseOut(e){
+//    
+//    e.target.classList.toggle("highlight");
+//    e.target.removeAttribute('style');
+//       }
 };
